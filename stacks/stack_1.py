6444836b35610237e209d1584e8e79490143fb03
Oladimeji <boladimeji834.com>
2025-06-04 05:33:58 +0100

    added all the elementary stack operations

--- stacks/stack_1.py
+++ stacks/stack_1.py
@@ -27,12 +27,64 @@ class Stack:
             return 
         for i in self.array[:self.top + 1]: 
             print(i, " ")
-            
 
+    # finction to check if the stack is full 
+    def is_full(self): 
+        return self.top + 1 == self.capacity
+            
+    # function to check if the stack is empty
+    def is_empty(self): 
+        return self.top == -1
+    
+    # function to look at the element on top of the stack
+    def peep(self): 
+        return self.array[self.top]
+
+    # funtion to count the number of elements on the stack
+    def count(self): 
+        return self.top + 1
+    
+    # function to get the element at the ith position
+    def peek(self, index): 
+        if self.top == -1: 
+            print("stack is empty, cannot peek")
+            return 
+
+        if self.top < index: 
+            print("stack index out of range, cannot peek")
+            return 
+        
+        return self.array[index]
+    # function to change the element at position i
+    def change(self, index, element): 
+        if self.is_empty(): 
+            print("stack is empty, no element to change!")
+            return 
+        
+        if self.top > index: 
+            print("stack index out of range, cannot change")
+            return 
+        
+        return self.array[index]
+    
 
 # let's test the stack 
 stack = Stack(capacity=5)
 stack.push(55)
 stack.push(66)
+stack.push(89)
+stack.push(15)
+stack.change(1, 99) # change the element at the 1th position (96 before)
 stack.pop()
+print(f"is the stack empty?: {stack.is_empty()}")
+print(f"is the stack full?: {stack.is_full()}")
+# element on top of the stack
+
+print(f"element on top of the stack: {stack.peep()}")
+print(f"element at position 2: {stack.peek(index=2)}")
+print(f"the number of elements on the stack: {stack.count()}")
+
+
+
+
 stack.display_elements()
